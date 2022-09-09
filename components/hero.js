2c0a9655afb9976a5d1b270c5189949c47200daa
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-09 17:00:47 +0900

    add components folder

---
+++ components/hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="t-MV__hero">
+      <h1>Portfolio</h1>
+      <p>Markup Engineer</p>
+      <h3>Mai Oshiro</h3>
+    </div>
+  );
+}
