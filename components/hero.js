eabcc03a880d730388bfa8c8abc0f26d47205667
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-12 17:37:05 +0900

    modified common layout

--- components/hero.js
+++ components/hero.js
@@ -3,10 +3,12 @@ import styles from "styles/hero.module.css";
 
 export default function Hero({ title, subtitle, text, imageOn = false }) {
   return (
-    <div className={styles.hero_header}>
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.subtitle}>{subtitle}</p>
-      <h3 className={styles.text}>{text}</h3>
+    <div className={styles.flexContainer}>
+      <div className={styles.hero_header}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+        <h3 className={styles.text}>{text}</h3>
+      </div>
       {imageOn && <figure>[画像]</figure>}
     </div>
   );
