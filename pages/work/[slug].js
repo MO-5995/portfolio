b4007ce30ffcfd45833aa34148d1ed68a694d958
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-17 10:38:58 +0900

    add routing

---
+++ pages/work/[slug].js
@@ -0,0 +1,87 @@
+import { getWorkBySlug, getAllSlugs } from "lib/api";
+import { extractText } from "lib/extract-text";
+import Meta from "@/components/meta";
+import Container from "@/components/container";
+import WorkHeader from "@/components/work-header";
+import WorkBody from "@/components/work-body";
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from "@/components/two-column";
+import ConvertBody from "@/components/convert-body";
+import WorkCategories from "@/components/post-categories";
+import Image from "next/image";
+import { getPlaiceholder } from "plaiceholder";
+import { eyecatchLocal } from "lib/constants";
+
+export default function Work({
+  title,
+  content,
+  eyecatch,
+  categories,
+  description,
+}) {
+  return (
+    <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <WorkHeader title={title} subtitle="Design Work" />
+      <figure>
+        <Image
+          src={eyecatch.url}
+          alt=""
+          layout="responsive"
+          width={eyecatch.width}
+          height={eyecatch.height}
+          sizes="(min-width:1152px) 1152px, 100vw"
+          priority
+          placeholder="blur"
+          blurDataURL={eyecatch.blurDataURL}
+        />
+      </figure>
+      <TwoColumn>
+        <TwoColumnMain>
+          <WorkBody>
+            <ConvertBody contentHTML={content} />
+          </WorkBody>
+        </TwoColumnMain>
+        <TwoColumnSidebar>
+          <WorkCategories categories={categories} />
+        </TwoColumnSidebar>
+      </TwoColumn>
+    </Container>
+  );
+}
+
+export async function getStaticPaths() {
+  const allSlugs = await getAllSlugs();
+  return {
+    paths: allSlugs.map(({ slug }) => `/work/${slug}`),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const slug = context.params.slug;
+  const work = await getWorkBySlug(slug);
+  const description = extractText(work.content);
+  const eyecatch = work.eyecatch ?? eyecatchLocal;
+  const { base64 } = await getPlaiceholder(eyecatch.url);
+  eyecatch.blurDataURL = base64;
+
+  return {
+    props: {
+      title: work.title,
+      content: work.content,
+      eyecatch: eyecatch,
+      categories: work.categories,
+      description: description,
+    },
+  };
+}
