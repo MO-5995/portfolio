abd5b59af138814ac142b1f0d9f3f6df015aed59
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-19 21:45:45 +0900

    feat: getAllPosts()

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,12 +1,22 @@
 import Meta from "@/components/meta";
 import Container from "@/components/container";
+import { getAllWorks } from "lib/api";
 import Hero from "@/components/hero";
 
-export default function Work() {
+export default function Work({ works }) {
   return (
     <Container>
       <Meta pageTitle="work" pageDesc="work list" />
       <Hero title="Work" subtitle="Recent works" />
     </Container>
   );
+}
+
+export async function getStaticProps() {
+  const works = await getAllWorks();
+  return {
+    props: {
+      works: works,
+    },
+  };
 }
