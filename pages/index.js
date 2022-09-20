68c98a49e2d3471f118fec417438371981ae3eb2
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-20 17:33:52 +0900

    feat: add works list on top page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Meta from "@/components/meta";
+import { getAllWorks } from "lib/api";
 import Container from "@/components/container";
 import Hero from "@/components/hero";
+import Works from "@/components/works";
+import Pagination from "@/components/pagination";
+import { getPlaiceholder } from "plaiceholder";
 
-export default function Home() {
+import { eyecatchLocal } from "lib/constants";
+
+export default function Home({ works }) {
   return (
     <Container>
       <Meta />
@@ -12,6 +18,24 @@ export default function Home() {
         text="Mai O."
         imageOn
       />
+      <Works works={works} />
+      <Pagination nextUrl="/work" nextText="More Works" />
     </Container>
   );
+}
+export async function getStaticProps() {
+  const works = await getAllWorks(4);
+
+  for (const work of works) {
+    if (!work.hasOwnProperty("eyecatch")) {
+      work.eyecatch = eyecatchLocal;
+    }
+    const { base64 } = await getPlaiceholder(work.eyecatch.url);
+    work.eyecatch.blurDataURL = base64;
+  }
+  return {
+    props: {
+      works: works,
+    },
+  };
 }
