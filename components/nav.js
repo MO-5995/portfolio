98c936ffa87523ae4fed666d7869c77f8d555a39
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-22 22:31:03 +0900

    feat:add hamburger menu

--- components/nav.js
+++ components/nav.js
@@ -1,23 +1,48 @@
+import { useState } from "react";
 import Link from "next/link";
 import styles from "styles/nav.module.css";
 
 export default function Nav() {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev);
+  };
+  const closeNav = () => {
+    setNavIsOpen(false);
+  };
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>
+          {`
+            @media (max-width: 767px) {
+              body {
+                overflow: hidden;
+                position: fixed;
+                width: 100%;
+              }
+            }
+          `}
+        </style>
+      )}
+      <button className={styles.btn} onClick={toggleNav}>
+        <span className={styles.bar}></span>
+        <span className="sr-only">MENU</span>
+      </button>
       <ul className={styles.list}>
         <li>
           <Link href="/">
-            <a>Home</a>
+            <a onClick={closeNav}>Home</a>
           </Link>
         </li>
         <li>
           <Link href="/about">
-            <a>About</a>
+            <a onClick={closeNav}>About</a>
           </Link>
         </li>
         <li>
           <Link href="/work">
-            <a>Work</a>
+            <a onClick={closeNav}>Work</a>
           </Link>
         </li>
       </ul>
