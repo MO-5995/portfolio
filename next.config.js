22d1c8deae177a8b881fae7435b047f40811c189
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-24 17:11:49 +0900

    feat:add security

--- next.config.js
+++ next.config.js
@@ -8,5 +8,24 @@ const nextConfig = {
   },
   swcMinify: true,
 };
+const headers = require("./headers");
+
+module.exports = (_) => {
+  return {
+    reactStrictMode: true,
+    eslint: {
+      dirs: ["src"],
+    },
+    async headers() {
+      return [
+        {
+          // 全てのパスに Security Headers を適用する
+          source: "/(.*)",
+          headers,
+        },
+      ];
+    },
+  };
+};
 
 module.exports = nextConfig;
